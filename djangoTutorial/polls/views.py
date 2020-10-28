605c10090216a763d5d10f446c43d1b737ea77a8
Alvaro Albero <aalbero@aquasinc.com>
2020-10-28 15:25:46 -0400

    feat: Add html form and voting capability

--- djangoTutorial/polls/views.py
+++ djangoTutorial/polls/views.py
@@ -1,8 +1,8 @@
-from polls.models import Question
-from django.http import HttpResponse
-from django.http import HttpRequest
-from django.http import Http404
+from django.http import HttpResponse, HttpResponseRedirect, HttpRequest
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+
+from .models import Question, Choice
 
 def index(request:HttpRequest):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -17,8 +17,22 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    return HttpResponse(f"You're looking at the results of question {question_id}.")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 
-def vote(request, question_id):
-    return HttpResponse(f"You're voting on question {question_id}.")
+def vote(request:HttpRequest, question_id):
+
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'quesetion': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
