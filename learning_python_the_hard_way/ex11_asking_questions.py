2c97eaf604927201cb536397b912e858de7cf359
albero94 <alvaro@quorum.us>
2020-11-17 18:40:04 -0500

    feat: Execises on strings, printing, user input and CL arguments

---
+++ learning_python_the_hard_way/ex11_asking_questions.py
@@ -0,0 +1,9 @@
+print "How old are you?"
+age = raw_input("--> ")
+height = raw_input("How tall are you? ")
+print "How much do you weight?",
+weight = raw_input()
+
+print "So you're %r old, %r tall, %r heavy." %(
+    age, height, weight
+)
