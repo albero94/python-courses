65ba17c0e639b12682959f5e368405d048a8b8f0
albero94 <ealbero94@gmail.com>
2021-02-15 18:33:32 -0500

    Unit tests

---
+++ learning_python_the_hard_way/ex47_automated_testing/setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    "description": "My Project",
+    "author": "Alvaro Albero",
+    "url": "https://alvaroalbero.com",
+    "download_url": "",
+    "author_email": "ealbero94@gmail.com",
+    "version": "0.1",
+    "install_requieres": ["nose"],
+    "packages": ["NAME"],
+    "scripts": [],
+    "name": "ex47"
+}
+
+setup(**config)
